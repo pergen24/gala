3a2752e0a759637ef6f126aa956cfc5df67d75b5
pergen24 <mbasitino@gmail.com>
2025-09-21 15:51:22 +0000

    Primera versión del proyecto Gala AEEEGES

--- app/config.py
+++ app/config.py
@@ -5,7 +5,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL',
-        'postgresql://pergentino:098765@db:5432/gala'
+#        'postgresql://pergentino:098765@db:5432/gala'
+         'postgresql+psycopg2://neondb_owner:npg_OZt6YRgW7ASF@ep-dry-meadow-adyj4mki-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+
     )
 
     # Carpeta para subir recibos
