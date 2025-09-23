26da5ac040edb58a5cca63229765c1115eb0177f
pergen24 <mbasitino@gmail.com>
2025-09-23 18:38:11 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 set -e
 
-HOST=${DB_HOST:-db}
-PORT=${DB_PORT:-5432}
-
-echo "Esperando a que PostgreSQL esté listo en $HOST:$PORT..."
-
-while ! nc -z $HOST $PORT; do
-  echo "Postgres no está listo todavía. Esperando 2s..."
-  sleep 2
-done
-
-echo "PostgreSQL está listo. Aplicando migraciones..."
-
-# Aplica migraciones
+echo "Aplicando migraciones..."
 flask db upgrade
 
 echo "Iniciando la app Flask..."
